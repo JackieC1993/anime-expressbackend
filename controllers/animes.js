d7105fada20f0a40b0d87e7ee9b2a8099ea461d2
JackieC1993 <jackiecheung@pursuit.org>
2024-06-25 14:02:06 -0400

    added more crud functionalty with Angie

--- controllers/animes.js
+++ controllers/animes.js
@@ -1,88 +1,153 @@
 const express = require("express");
 const animes = express.Router();
 const animesArr = require("../data/animes");
+const { getAllAnimes, getOneAnime, createAnime, updateAnime, deleteAnime } = require("../queries/animes");
 
-// Read All - Index hhtp verb: Get CRUD Read- get a list of all animes
+// Read All - Index http verb: GET, CRUD: Read - get a list of all animes
 
-animes.get("/", (req, res) => {
-  // " /animes" and is a get request
+animes.get("/", async (_, res) => {
+  // "/animes/" and is a get request
   try {
-    res.status(200).json(animesArr);
+    const animes = await getAllAnimes();
+    res.status(200).json(animes);
   } catch (error) {
     res.status(404).json({ error: error });
   }
 });
 
-// get one anime
+// get ONE Anime
 // http method - get
-// we need the id  of the specific anime we are trying to get
+// we need the id of the specific anime we are trying to get
 // path : "/animes/:id"
-animes.get("/:id", (req, res) => {
+animes.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    // since req.params is an object we can destructure the param we are in need of
-    const anime = animesArr.find((anime) => anime.id === Number(id));// if find does not find an element that meets the conditonal it returns undefined
-    if (anime){ // if anime is truthy (it is undefined)
-        res.status(200).json(anime);
-    } else {
-       throw "could not find anime"; // if the anime is not found we forced an error to land in the catch and responds with the error
-    }
+    const anime = await getOneAnime(id);
+    res.status(200).json(anime)
   } catch (error) {
     res.status(404).json({ error: error });
   }
 });
+
 // create one anime
-//http method is a post
+// http method is POST
 // path is /animes
 
-animes.post("/", (request, response) => {
-    try {
-        const anime = request.body; //store the new anime data in the anime variable
-        if (anime.id) { //if data exist add to animes array and send back successful response
-            animesArr.push(anime);
-            response.status(201).json(animesArr[animesArr.length - 1]);
-        } else { //Otherwise trigger an error message
-            throw "Could not create anime, please provide data";
-        }
-    } catch (error) {
-        response.status(400).json({ error: error })
+animes.post("/", async (req, res) => {
+  try {
+    const anime = req.body; // store the new anime data in the anime variable
+    const newAnime = await createAnime(anime)
+    res.status(201).json(newAnime)
+  } catch (error){
+    res.status(404).json({error: error})
+  }
+});
 
-    }
-})
-//update an anime
-// http method
+// update an anime
+// http method PUT
 // path is /animes/:id
 
-animes.put("/:id", (req, res) => {
+animes.put("/:id", async (req, res) => {
   try {
-    const { id } = req.params; // destructuring the id of the anime we are updating
+    const { id } = req.params; // desctructure the id of the anime we are updating
     const anime = req.body; // req.body provides the anime data with updates
-    const index = animesArr.findIndex((anime) => anime.id === Number(id)); // we are using the id to find the anime we are replacing. If findIndex() does not find the anime it returns -1, otherwise it returns the index.
-    if (index !== -1) { // if it found the index
-      animesArr.splice(index, 1, anime);// replace the anime with the updated anime object
-      res.status(201).json(animesArr[index]); // sending back a successful response
-    } else { // otherwise we trigger an error
-      throw "could not update anime"; // triggering the error
-    }
+    const updatedAnime = await updateAnime(id, anime)
+      res.status(201).json(updatedAnime); // sending back a successful response
   } catch (error) {
     res.status(400).json({ error: error });
   }
 });
-// delete an anime
+
+// delete one anime
 // http method is delete
 // path = /animes/:id
-animes.delete("/:id", (req, res) => {
+
+animes.delete("/:id", async (req, res) => {
   try {
-    const {id} = req.params; // destructuring the id of the anime we're deleting
-    const index = animesArr.findIndex(anime => anime.id === Number(id)); // we are finding that anime using the id. If it is nit found findIndex() returns -1
-    if (index !== -1){ // if the index is found
-        animesArr.splice(index, 1); // we will delete the anime
-        res.status(200).json(animesArr);// sending a successful response and our updated array
-    }else{ // otherwise we trigger an error
-        throw "could not find animes"
-    }
+    const { id } = req.params; // destructuring the id of the anime we're deleting
+    const deletedAnime = await deleteAnime(id)
+    res.status(200).json(deletedAnime)
   } catch (error) {
-    res.status(404).json({error: error});
+    res.status(404).json({ error: error });
   }
 });
+
 module.exports = animes;
+
+
+
+
+
+
+
+
+
+// to get one id
+// animes.get("/:id", (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     // since req.params is an object we can destructure the param we are in need of
+//     const anime = animesArr.find((anime) => anime.id === Number(id)); // if find does not find an element that meets the conditional it returns undefined
+//     if (anime) {
+//       // if anime is truthy (it is not undefined)
+//       res.status(200).json(anime);
+//     } else {
+//       throw "could not find anime"; // if the anime is not found we forced an error to land in the catch and respond with the error
+//     }
+//   } catch (error) {
+//     res.status(404).json({ error: error });
+//   }
+// });
+// post an anime 
+// animes.post("/", (req, res) => {
+//   try {
+//     const anime = req.body; // store the new anime data in the anime variable
+
+//     if (anime.id) {
+//       // if data exist add to animes arr and send back successful response
+//       animesArr.push(anime);
+//       res.status(201).json(animesArr[animesArr.length - 1]);
+//     } else {
+//       // otherwise trigger an error message
+//       throw "could not create. please provide data";
+//     }
+//   } catch (error) {
+//     res.status(400).json({ error: error });
+//   }
+// });
+
+//Update our anime
+// animes.put("/:id", (req, res) => {
+//   try {
+//     const { id } = req.params; // desctructure the id of the anime we are updating
+//     const anime = req.body; // req.body provides the anime data with updates
+//     const index = animesArr.findIndex((anime) => anime.id === Number(id)); // we are using the id to find the anime we are replacing. If findIndex() does not find the anime it returns -1, otherwise it returns the index
+//     if (index !== -1) {
+//       // if it found the index
+//       animesArr.splice(index, 1, anime); // replace the anime with the updated anime object
+//       res.status(201).json(animesArr[index]); // sending back a successful response
+//     } else {
+//       // otherwise we trigger an error
+//       throw "could not update anime"; // triggering the error
+//     }
+//   } catch (error) {
+//     res.status(400).json({ error: error });
+//   }
+// });
+// delete the animes -------------
+// animes.delete("/:id", (req, res) => {
+//   try {
+//     const { id } = req.params; // destructuring the id of the anime we're deleting
+//     const index = animesArr.findIndex((anime) => anime.id === Number(id)); // we are finding that anime using the id. If it is not found findIndex() returns -1
+//     if (index !== -1) {
+//       // if the index is found
+//       animesArr.splice(index, 1); // we will delete the anime
+//       res.status(200).json(animesArr); // sending a successful response and our updated array
+//     } else {
+//       // otherwise we trigger an error
+//       throw "could not find animes";
+//     }
+//   } catch (error) {
+//     res.status(404).json({ error: error });
+//   }
+// });
--- queries/animes.js
+++ queries/animes.js
@@ -16,9 +16,40 @@ const getOneAnime = async (animeId) => {
     } catch (error) {
         return error
     }
+};
+const createAnime = async ({name, description}) => {
+    try {
+        const newAnime = await db.one("INSERT INTO animes (name, description) VALUES ($1, $2) RETURNING *",[name, description])
+        return newAnime
+    } catch (error) {
+        return error
+    }
+};
+
+const updateAnime = async (animeId, anime) => {
+    try {
+        const updatedAnime = db.one("UPDATE animes SET name =$1, description=$2 WHERE id=$3 RETURNING *",[anime.name, anime.description,animeId])
+        return updatedAnime
+    } catch (error) {
+        return error
+    }
 }
+
+const deleteAnime = async (animeId) => {
+    try {
+        const deletedAnime = await db.one("DELETE from animes WHERE id=$1 RETURNING *", animeId);
+        return deletedAnime
+    } catch (error) {
+        return error
+    }
+}
+
+
 
 module.exports = {
     getAllAnimes,
-    getOneAnime
+    getOneAnime,
+    createAnime,
+    updateAnime,
+    deleteAnime
 }
